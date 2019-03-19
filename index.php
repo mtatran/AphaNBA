158ef4f38f3e05208a88f666c1ccf673d1c0d48c
Melissa Tran <30379133+mtatran@users.noreply.github.com>
2019-03-19 17:53:04 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,308 @@
+
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta name="robots" content="noindex, nofollow">
+  <meta name="googlebot" content="noindex, nofollow">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+  <script type="text/javascript" src="/js/lib/dummy.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="/css/result-light.css">
+
+      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <link rel="stylesheet"
+       href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css">
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+  <a href="https://icons8.com"></a>
+
+      <script type="text/javascript" src="https://fonts.googleapis.com/css?family=Roboto|Karma|Coiny|Gudea|Open+Sans|EB+Garamond|Asap|Spectral+SC"></script>
+      <script type="text/javascript" src="<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>"></script>
+			<script>
+			$(document).ready(function(){
+				$('.modal').modal();
+			});
+			  $('.dropdown-trigger').dropdown();
+			</script>
+</head>
+
+
+  <style type="text/css">
+    .ad-container {
+  width: 100%;
+}
+html *{
+	padding-left:20px;
+}
+.ad {
+  border: 1px solid black;
+  width: 32.2%;
+  /*Adjust as necessary*/
+  margin: .3%;
+  float: left;
+  /*Add min-height or max-height as necessary*/
+}
+/* Mobile Styles */
+@media only screen and (max-width: 400px) {
+  body {
+    background-color: #white;
+    .container{
+   width: 33.3%;
+   height:33.3%/* Red */
+  }
+}
+/* Tablet Styles */
+@media only screen and (min-width: 401px) and (max-width: 960px) {
+  body {
+    background-color: #white
+    .container{
+   width: 66.6%;
+   height: 66.6%;
+ }
+    /* Yellow */
+  }
+}
+/* Desktop Styles */
+@media only screen and (min-width: 961px) {
+  body {
+    background-color: #white; /* Blue */
+  }
+}
+.ad-container:after {
+  display: table;
+  content: "";
+  clear: both;
+}
+h1 {
+  font-family: 'Roboto', serif;
+  font-size: 52px;
+  font-weight: 400;
+  color: black;
+}
+p {
+  font-family: "Roboto", serif;
+  font-size: 14px;
+	color:orange;
+}
+p1 {
+  font-family: "Roboto", serif;
+  font-size: 10px;
+  color: black
+}
+.boldlet {
+  font-family: 'Spectral SC', serif;
+  font-size: 48px;
+}
+h2 {
+  font-family: 'Roboto', serif;
+  font-size: 36px;
+  font-weight: 400;
+  color: black;
+}
+.image {
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: #C1C1C1;
+}
+.text {
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+  font-family: "Roboto", serif;
+  font-size: 14px;
+  }
+}
+.carousel-slider:hover .overlay {
+  opacity: 1;
+}
+/* just for jsfiddle */
+.middle-indicator{
+   position:absolute;
+   top:50%;
+   }
+  .middle-indicator-text{
+   font-size: 4.2rem;
+  }
+  a.middle-indicator-text{
+    color:white !important;
+  }
+.content-indicator{
+    width: 64px;
+    height: 64px;
+    background: none;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+  }
+p3{
+font-family:"Roboto",serif;
+font-size:11px;
+color:black;
+}
+    .indicators{
+  	visibility: hidden;
+  }
+.fake-div {
+  width: 100%;
+  position: relative;
+}
+.tabs-content.carousel .carousel-item{height:800px}
+.dot {
+    height: 50px;
+    width: 50px;
+    background-color: #bbb;
+    border-radius: 50%;
+    display: inline-block;
+}
+.carousel .carousel-item {
+height:600px !important;}
+.txt:hover {
+    text-decoration: underline;
+}
+/* Parts are from twitter.com */
+@-webkit-keyframes typing { from { width: 0; } }
+@-webkit-keyframes blink-caret { 50% { border-color: transparent; } }
+h1 {
+	font: bold 300% Consolas, Monaco, monospace;
+	border-right: .1em solid black;
+	width: 16.5em;
+	width: 21ch;
+	margin: 2em 1em;
+	white-space: nowrap;
+	overflow: hidden;
+	-webkit-animation: typing 10s steps(21, end),
+	           blink-caret .5s step-end infinite alternate;
+}
+.HeartAnimation {
+  padding-top: 2em;
+  background-image: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/66955/web_heart_animation.png');
+	background-repeat: no-repeat;
+	background-size: 2900%;
+  background-position: left;
+	height: 50px;
+	width: 50px;
+  animation: heart-burst 5s steps(28) infinite;
+  margin: 0 auto;
+}
+@keyframes heart-burst {
+	0% {
+		background-position: left
+	}
+	100% {
+		background-position: right
+	}
+}
+  </style>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+    <title>Untitled Document</title>
+    <meta name="description" content="">
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+
+<body>
+
+<h1>Dashboard</h1>
+<?php
+// Enable error logging:
+error_reporting(E_ALL ^ E_NOTICE);
+// mysqli connection via user-defined function
+include ('./my_connect.php');
+$mysqli = get_mysqli_conn();
+?>
+
+<?php
+// SQL statement
+$sql = "SELECT t.teamName, t.team_Id "
+	. "FROM team t, works_in wi "
+	. "WHERE wi.member_Id=111111 AND wi.team_Id=t.team_Id";
+// Prepared statement, stage 1: prepare
+$stmt = $mysqli->prepare($sql);
+// Prepared statement, stage 2: execute
+$stmt->execute();
+// Bind result variables
+$stmt->bind_result($team_name,$team_Id);
+/* fetch values */
+echo '<label for="aid">Your Team(s)</label>';
+echo '<a href="index2.php">Join</a>';
+echo '<a href="createteam.php">Create</a>';
+echo '<div name="aid">';
+while ($stmt->fetch())
+{
+  printf ('<a href="something.php?team_name='.$team_name.',team_id='.$team_Id.'" >%s</a>', $team_name);
+}
+echo '</div><br>';
+echo '</p>';
+/* close statement and connection*/
+$memberId = 111111;
+// SQL statement
+$sql = "SELECT p.project_name, p.projects_Id, p.documentation "
+	. "FROM work_on wo, projects p "
+	. "WHERE wo.member_Id=".$memberId." AND wo.projects_Id=p.projects_Id";
+$stmt = $mysqli->prepare($sql);
+$stmt->execute();
+$stmt->bind_result($project_name,$project_id,$project_documentation);
+echo '<label for="aid">Your Projects and Tasks</label>';
+echo '<a href="createproject.php">Create project</a>';
+echo '<div name="aid">';
+$something = array();
+while ($stmt->fetch())
+{
+	$something[$project_id] = $project_name;
+}
+foreach($something as $id => $name) {
+	$projectSql = "SELECT t.taskName"
+	." FROM in_task it,has_task ht,task t"
+	." WHERE ht.task_Id=it.task_Id AND it.member_Id=".$memberId." AND ht.projects_Id=".$id." AND it.task_id=t.task_id";
+	$projectStmnt = $mysqli->prepare($projectSql);
+	$projectStmnt->execute();
+	$projectStmnt->bind_result($task_name);
+	echo '<p>';
+	printf ('<div>%s</div>', $name);
+  echo '<a class="waves-effect waves-light btn modal-trigger" href="projectoverview.php?project_id='.$id.'">+</a>';
+	echo '</p>';
+	echo '<p3>';
+	echo '<ul>';
+	while($projectStmnt->fetch()) {
+		printf('<li>%s</li>', $task_name);
+	}
+	echo '</ul>';
+echo '</p3>';
+}
+echo '</div><br>';
+/* close statement and connection*/
+$stmt->close();
+$mysqli->close();
+?>
+
+
+<br>
+
+<div class="HeartAnimation">
+</div>
+</body>
