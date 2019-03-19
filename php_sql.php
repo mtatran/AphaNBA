bdc51dc5f7f7afb04f3e1622d08516810ea26e1b
Melissa Tran <30379133+mtatran@users.noreply.github.com>
2019-03-19 17:51:26 -0400

    add files

---
+++ php_sql.php
@@ -0,0 +1,41 @@
+<?php
+// (1) Connection parameters
+$dbhost = 'mansci-db.uwaterloo.ca';
+$dbuser = 'mtatran';
+$dbpassword = 'passwordmel';
+$dbname = 'mtatran';
+// (2) mysqli connection variable
+$mysqli = new mysqli($dbhost, $dbuser, $dbpassword, $dbname);
+if ($mysqli->connect_errno)
+{
+echo 'Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
+}
+echo '<p>' . $mysqli->host_info . '</p>';
+// (3) SQL statement
+$sql = "SELECT a.aid, a.aname "
+	. "FROM aircraft a "
+	. "WHERE a.aname LIKE CONCAT(?, '%')";
+	// LIKE with prepared statement
+// (4a) Prepared statement, stage 1: prepare
+$stmt = $mysqli->prepare($sql);
+// (4b) Bind a PHP variable, $search as a string parameter
+$search = 'Boeing';
+// "i" for integer, "d" for double, "s" for string, "b" for blob
+$stmt->bind_param('s', $search);
+// (5) Execute prepared statement
+$stmt->execute();
+// (6) Bind selected columns to PHP variables
+$stmt->bind_result($aircraft_id, $aircraft_name);
+// (7) fetch values
+// <ul> is unordered list
+echo '<ul>';
+while ($stmt->fetch())
+{
+// printf is print format, <li> is list item
+printf ('<li>%s (%s)</li>', $aircraft_name, $aircraft_id);
+}
+echo '</ul>';
+// (8) close statement and mysqli connection
+$stmt->close();
+$mysqli->close();
+?>
