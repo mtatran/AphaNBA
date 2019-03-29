6db661846d41abe603a78bee79c3d383c51b5ce9
Anna <annajamtorki@gmail.com>
2019-03-29 16:30:04 -0400

    u

--- nba-design2/compareTeams.php
+++ nba-design2/compareTeams.php
@@ -206,7 +206,7 @@ printf ( $home);
   //  echo '<p>';
   printf ($home);
     //echo '</p>';
-  $sql = "SELECT t.win "
+  $sql = "SELECT "
   . "FROM teams t "
   . "WHERE t.team_name='".$home."'";
   $search_result = mysqli_query($mysqli, $sql);
