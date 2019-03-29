2bb649d960cec5a5ef9dbf82cc5b309296fc01c8
Anna <annajamtorki@gmail.com>
2019-03-29 15:54:27 -0400

    u

--- nba-design2/compareTeams.php
+++ nba-design2/compareTeams.php
@@ -196,6 +196,11 @@ printf ( $home);
   $sql = "SELECT t.win "
   . "FROM teams t "
   . "WHERE t.team_name='".$home."'";
+  $search_result = mysqli_query($mysqli, $sql);
+  while ($row = mysqli_fetch_assoc($search_result))
+         {
+            $loss = $row[loss];
+         }
   $sql2 = "SELECT t.loss "
   . "FROM teams t "
   . "WHERE t.team_name='".$home."'";
