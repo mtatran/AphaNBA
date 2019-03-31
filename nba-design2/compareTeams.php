721a7c1329ee3df09cef5173208f1757864a2ef1
Anna <annajamtorki@gmail.com>
2019-03-31 12:42:13 -0400

    u

--- nba-design2/compareTeams.php
+++ nba-design2/compareTeams.php
@@ -79,11 +79,8 @@ printf ( $home);
           ?>
           ,       <?php
                 $date_time = isset($_GET['date_time']) ? $_GET['date_time'] : false;
-
-                if($date_time){
                 echo date_format($date_time, 'g:ia \o\n l jS F Y');
-
-                }?></h5>
+                ?></h5>
       </div>
       <div align="center">
         <table class="striped" style>
