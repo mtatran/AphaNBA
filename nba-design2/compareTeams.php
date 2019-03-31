5a4b030dc8b9e5b2fb908efdcfe742d63c16ced8
Anna <annajamtorki@gmail.com>
2019-03-31 13:07:04 -0400

    u

--- nba-design2/compareTeams.php
+++ nba-design2/compareTeams.php
@@ -72,7 +72,7 @@ printf ( $home);
 
 
           if($game_location) {
-
+          echo '<br>';
           printf ($game_location);
 
           }
@@ -80,7 +80,6 @@ printf ( $home);
          <?php
                 $date_time = isset($_GET['date_time']) ? $_GET['date_time'] : false;
                 $date = date_create($date_time);
-                echo '<br>';
                 echo '<br>';
                 echo date_format($date, 'l \, F jS Y \a\t g:ia'),' EST';
                 ?></h5>
